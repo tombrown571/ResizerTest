9c5b947063ffca0108296507a246d6890e4ec44a
Tom Brown <tom.brown@purplebricks.com>
2017-01-20 16:12:39 +0000

    placeholder class for FreeImageCore

---
+++ ResizerTestConsole/ResizerTestConsole/FreeImageCorePackageTester.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResizerTestConsole
+{
+
+    /// <summary>
+    /// https://www.nuget.org/packages/FreeImage-dotnet-core
+    /// Install-Package FreeImage-dotnet-core
+    /// </summary>
+    public class FreeImageCorePackageTester : ImagePackageTesterBase
+    {
+        public FreeImageCorePackageTester(string imagePath, string outputDir) : base(imagePath, outputDir)
+        {
+        }
+
+        public override bool ProcessImage(string outputName, int maxDimension)
+        {
+            bool isValid = false;
+            try
+            {
+                var outputPath = Path.Combine(_outputDir, outputName + ".jpg");
+
+                //FreeImageAPI.FreeImage input = new FreeImageAPI.FreeImageBitmap();
+
+                // TODO finish FreeImageCore resize 
+
+                isValid = true;
+            }
+            catch (Exception ex)
+            {
+                Exceptions.Add(new ErrorLogStruct()
+                {
+                    Ex = ex,
+                    LogText = string.Format("File: {0}, Dimension: {1}", _imagefileName, maxDimension),
+                });
+            }
+            return isValid;
+        }
+    }
+}
